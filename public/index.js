cbe65e0ab02700e8906f8990bfc9f89b1cf4f39e
friesemi <35504972+friesemi@users.noreply.github.com>
2019-06-04 16:13:26 -0700

    Navbar items now have some interaction

--- public/index.js
+++ public/index.js
@@ -1,13 +1,22 @@
 //add js here for index page
 
 var navItems = document.getElementsByClassName('navitem');
+for (var i = 0; i < navItems.length; i++) {
+    navItems[i].addEventListener('click', handleNavItemClicked);
+}
 
-navItems.addEventListener('click', function (event) {
-    //get name of tag and open corresponding page
+
+function handleNavItemClicked(event){
+//get name of tag and open corresponding page
     if (event.target.textContent == 'Game')
-        console.log("Open the game page (home page)");
+        //res.status(200).render('gamePage');
+        console.log('game page gets loaded');
+
     else if (event.target.textContent == 'Rules')
-        console.log("Open the rules page");
+        //res.status(200).render('rulesPage');
+        console.log('rules page gets loaded');
+
     else if (event.target.textContent == 'Customize')
-        console.log("Open the customization page");
-});
+        //res.status(200).render('customizePage');
+        console.log('customize page gets loaded');
+    }
