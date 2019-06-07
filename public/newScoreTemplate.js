2d00ffb4abf125fcd998497514472968ba13a5e0
friesemi <35504972+friesemi@users.noreply.github.com>
2019-06-07 15:01:22 -0700

    changed score page slightly

--- public/newScoreTemplate.js
+++ public/newScoreTemplate.js
@@ -3,10 +3,10 @@
 templates['newScore'] = template({"compiler":[7,">= 4.0.0"],"main":function(container,depth0,helpers,partials,data) {
     var helper, alias1=depth0 != null ? depth0 : (container.nullContext || {}), alias2=helpers.helperMissing, alias3="function", alias4=container.escapeExpression;
 
-  return "<li>\r\n    <div class=\"score\">\r\n        <p class=\"scoreName\">\r\n            "
+  return "<li>\r\n    <div class=\"score\">\r\n        <h2 class=\"scoreName\">\r\n            "
     + alias4(((helper = (helper = helpers.name || (depth0 != null ? depth0.name : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"name","hash":{},"data":data}) : helper)))
     + ": "
     + alias4(((helper = (helper = helpers.score || (depth0 != null ? depth0.score : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"score","hash":{},"data":data}) : helper)))
-    + "\r\n            <button class=\"deleteButton\">X</button>\r\n        </p>\r\n    </div>\r\n</li>";
+    + "\r\n            <button class=\"deleteButton\">DELETE</button>\r\n        </h2>\r\n    </div>\r\n</li>";
 },"useData":true});
 })();
