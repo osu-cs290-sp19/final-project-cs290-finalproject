847b716f5840c528c459e4e56859cda02875d273
friesemi <35504972+friesemi@users.noreply.github.com>
2019-06-12 13:12:16 -0700

    naming mistake

--- server.js
+++ server.js
@@ -48,7 +48,7 @@ app.get('/', function (req, res, next) {
     res.status(200).render('gamePage');
     var diceCollection = db.collection('dice');
     diceCollection.drop();
-    if(dice.length <= 0)
+    if(diceCollection.length <= 0)
         loadDice(diceCollection);
     var diceArray = diceCollection.find();
     diceArray.toArray(function (err, diceArr) {
