dd4b3c3ef581451dcd71bf18f484e9ffc3013907
hahn2014 <slapshot1998@gmail.com>
2019-06-13 00:52:34 -0700

    Added random 5 dice roll function, and I think got the images of the dice working too.

--- README.md
+++ README.md
@@ -2,12 +2,8 @@
 ### Developped by Bryce Hahn and Michael Friesen
 
 ## What to expect:
-> A really fun card game of Hearts to play against 3 bots!
+> A really fun dice game!
 > - Bryce Hahn, Michael Friesen
-
-## How will this benefit the world:
-> Card games are a fun way to either fix a boring night or fun with the family, but with our website when you're all alone you can play this instead :)
-> - Bryce Hahn
 
 ## Our Timeline:
 - [X] Server initial framework
@@ -18,13 +14,11 @@
 * 	ETA: June 7, 2019
 - [X] Styling and basic JS functionality implemented
 * 	ETA: June 7, 2019
-- [ ] CSS Layout Done
+- [X] CSS Layout Done
 * 	ETA: June 8, 2019
+- [X] Game logic completed
+* 	ETA: June 9, 2019
 - [ ] Database communication implementation
-* 	ETA: June 9, 2019
-- [ ] Game logic completed
-* 	ETA: June 9, 2019
-- [ ] Bot logic completed
-* 	ETA: June 10, 2019
+* 	ETA: June 13, 2019
 - [ ] Project finished
 * 	ETA: June 14, 2019
--- public/index.js
+++ public/index.js
@@ -1,3 +1,5 @@
+var roundScore = 0;
+
 //Code for dice game
 window.addEventListener('DOMContentLoaded', function () {
     //if the roll button is clicked this adds the dice
@@ -9,9 +11,21 @@ window.addEventListener('DOMContentLoaded', function () {
 
 //this rolls and renders the dice
 function handleDiceRoll() {
+    var rolls = [];
 
+    for (var i = 0; i < 5; i++) {
+        rolls[i] = getRandomInt(5) + 1;
+        var curDie = "dice" + (i + 1);
+        document.getElementById(curDie).src = "./dice/" + (i + 1) + ".PNG";
+        document.getElementById(curDie).classList.remove("inactive");
+        roundScore += rolls[i];
+    }
+    alert("Nice! You scored a " + roundScore+ "! Play Again!");
 }
 
+function getRandomInt(max) {
+  return Math.floor(Math.random() * Math.floor(max));
+}
 
 function handleScoreSave() {
     var name = document.getElementById('name-input').value.trim();
@@ -20,7 +34,6 @@ function handleScoreSave() {
         alert("Must provide a name!");
     }
     else {
-
         var postReq = new XMLHttpRequest();
         var reqUrl = '/scores/addScore';
         postReq.open('POST', reqUrl);
